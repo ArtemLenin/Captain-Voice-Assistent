7ab2859b8f913eafc8dc0e393f3481638c99cfa4
artem_grabovsky <artemlenin96@gmail.com>
2022-06-16 10:25:16 +0500

    Add project files

---
+++ config.py
@@ -0,0 +1,20 @@
+names = ['капитан', "кэп", "кап", "товарищ", "старпом", "помощник"]
+user_name = 'милорд'
+
+vk_token = '5dde59846ae4bf495250ef464bb023cf465dbf95e55fb4d3351eef69b94c378c4cc8767dd3c750084e12e'
+vk_id = 63475836
+
+weather_token = 'a54f343a58742cd631414510269121da'
+weather_city = 'Tyumen'
+
+month = [
+        "Января", "Февраля", "Марта", "Апреля",
+        "Мая", "Июня", "Июля", "Августа", "Сентября",
+        "Октября", "Ноября", "Декабря"
+    ]
+
+
+jokes_url = 'http://anekdotme.ru/random'
+
+alarm_status = False
+alarm_time = ''
