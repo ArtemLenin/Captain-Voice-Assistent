d1ac2a9bdb957ea76ba8fbe51361634bcd96d79f
ArtemLenin <artemlenin96@gmail.com>
2022-06-16 10:48:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 names = ['капитан', "кэп", "кап", "товарищ", "старпом", "помощник"]
 user_name = 'милорд'
 
-vk_token = '5dde59846ae4bf495250ef464bb023cf465dbf95e55fb4d3351eef69b94c378c4cc8767dd3c750084e12e'
-vk_id = 63475836
+vk_token = ''
+vk_id = 
 
-weather_token = 'a54f343a58742cd631414510269121da'
-weather_city = 'Tyumen'
+weather_token = ''
+weather_city = ''
 
 month = [
         "Января", "Февраля", "Марта", "Апреля",
